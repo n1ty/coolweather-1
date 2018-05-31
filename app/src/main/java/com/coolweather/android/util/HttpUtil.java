d5aa97636b181717e24eee090ed270e8e6ca8c87
wuting77 <2356438053@qq.com>
2018-05-31 18:40:08 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,29 @@
+package com.coolweather.android.util;
+
+import android.app.DownloadManager;
+import android.app.VoiceInteractor;
+
+import com.bumptech.glide.request.Request;
+
+import okhttp3.OkHttpClient;
+
+
+
+
+/**
+ * Created by admin on 2018/5/31.
+ */
+
+public class HttpUtil {
+    public static void sendOKHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+       // okhttp3.Request request=new Request.Builder().url(address).build();
+       okhttp3.Request request = new okhttp3.Request.Builder()
+                .url(address)
+                .build();
+        //okhttp3.Request request=new
+        client.newCall(request).enqueue(callback);
+
+
+    }
+}
